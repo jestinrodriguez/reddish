51a191e7dbc164269f94ea2183beea7b1190f9ca
Jestin Rodriguez <40747173+jestinrodriguez@users.noreply.github.com>
2025-04-25 10:35:15 -0700

    building home page

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -1,10 +1,30 @@
+import PostsList from "@/components/post/PostsList";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hello world</h1>
-      <Button>hello</Button>
-    </div>
+    <>
+      {/* Banner */}
+      <section>
+        <div className="mx-auto max-w-7xl px-4 py-6">
+          <div className="flex items-center">
+            <div>
+              <h1 className="text-2xl font-bold">Home</h1>
+              <p className="text-sm text-gray-600">
+                Recent post from all communities
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* Post */}
+      <section className="my-8">
+        <div className="mx-auto max-w-7xl px-4">
+          <div className="flex flex-col gap-4">
+            <PostsList/>
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
---
+++ components/post/PostsList.tsx
@@ -0,0 +1,17 @@
+import { getPosts } from '@/sanity/lib/post/getPosts';
+import { currentUser } from '@clerk/nextjs/server';
+import React from 'react'
+
+const PostsList = async () => {
+    const posts = await getPosts();
+    const user = await currentUser();
+  return (
+    <div className="space-y-4">
+        {posts.map((post) => (
+            <Post key={post._id} post={post} userId={user?.id || null} />
+        ))}
+    </div>
+  )
+}
+
+export default PostsList
